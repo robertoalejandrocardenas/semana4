4b0d8a4f49022725af25fea729a31cbf21ff1202
cardenasroberto <101744240+cardenasroberto@users.noreply.github.com>
2022-09-28 11:01:58 -0500

    Add files via upload

---
+++ CCOMP/arreglos/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int array[5];
+    array[0]=5;
+    array[1]=4;
+    array[2]=3;
+    array[3]=80;
+    array[4]=11;
+
+ for(  int i=0; i<5; i++)
+ cout<< array[i] << endl;
+    /*cout<< array[0] << endl;
+    cout<< array[1] << endl;
+    cout<< array[2] << endl;
+    cout<< array[3] << endl;
+    cout<< array[4] << endl;*/
+
+return 0;
+};
